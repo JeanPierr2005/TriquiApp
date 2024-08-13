64751133fb01e0bdbdae96430c0242170d98ea3e
Jean Pierr Valencia <jean.valencia@softwareone.com>
2024-08-13 16:40:59 -0500

    Create Server

---
+++ backEnd/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+server.listen(3000, () => {
+  console.log("Server esta en el puerto 3000");
+});
+io.on("connection", (socket) => {
+  console.log("Nueva Conexion", socket);
+});
